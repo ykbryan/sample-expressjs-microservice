39040c0ad24a96db67bfd1560b9c88dee25cb293
Bryan Chua <me@bryanchua.com>
2018-10-18 13:55:10 -0700

    Adding sample restful apis for products

---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express')
+
+// declare a new express app
+const app = express()
+const port = 3000
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
+/**********************
+ * Example get method *
+ **********************/
+
+app.get('/products', function (req, res) {
+  // Add your code here
+  // Return the API Gateway event and query string parameters for example
+  res.json({ success: 'get call succeed!', url: req.url });
+});
+
+app.get('/products/*', function (req, res) {
+  // Add your code here
+  res.json({ success: 'get call succeed!', url: req.url });
+});
+
+/****************************
+* Example post method *
+****************************/
+
+app.post('/products', function (req, res) {
+  // Add your code here
+  res.json({ success: 'post call succeed!', url: req.url, body: req.body })
+});
+
+app.post('/products/*', function (req, res) {
+  // Add your code here
+  res.json({ success: 'post call succeed!', url: req.url, body: req.body })
+});
+
+/****************************
+* Example post method *
+****************************/
+
+app.put('/products', function (req, res) {
+  // Add your code here
+  res.json({ success: 'put call succeed!', url: req.url, body: req.body })
+});
+
+app.put('/products/*', function (req, res) {
+  // Add your code here
+  res.json({ success: 'put call succeed!', url: req.url, body: req.body })
+});
+
+/****************************
+* Example delete method *
+****************************/
+
+app.delete('/products', function (req, res) {
+  // Add your code here
+  res.json({ success: 'delete call succeed!', url: req.url });
+});
+
+app.delete('/products/*', function (req, res) {
+  // Add your code here
+  res.json({ success: 'delete call succeed!', url: req.url });
+});
+
+app.listen(port, function () {
+  console.log("App started")
+});
+
+// Export the app object.
+// module.exports = app
